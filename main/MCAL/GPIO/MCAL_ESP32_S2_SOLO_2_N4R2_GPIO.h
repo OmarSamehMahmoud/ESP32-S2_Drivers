bb59ae338ac06230d9751a8f4cb7a56455dfa737
Omar Sameh Mahmoud <62811657+OmarSamehMahmoud@users.noreply.github.com>
2025-05-10 11:51:32 +0300

    Update MCAL_ESP32_S2_SOLO_2_N4R2_GPIO.h

--- main/MCAL/GPIO/MCAL_ESP32_S2_SOLO_2_N4R2_GPIO.h
+++ main/MCAL/GPIO/MCAL_ESP32_S2_SOLO_2_N4R2_GPIO.h
@@ -6,8 +6,6 @@
  Device(s)      : ESP32_S2_SOLO_2_N4R2
  Creation Date  : 16/08/2024
  Testing Date   : 
- @COPYRIGHT 2016 El-ARABY Research and development center.
- all rights reserved
 *********************************************************************************************************************************/
 #ifndef MCAL_ESP32S2_S2_SOLO_2_N4R2_GPIO_H_
 #define MCAL_ESP32S2_S2_SOLO_2_N4R2_GPIO_H_
